1f31c079987db92413c21d4d9a37669c041ce849
shipra-kushwaha <shiprak93@gmail.com>
2020-08-22 16:06:40 +0530

    shipra

---
+++ include/header.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="shortcut icon" href="img/logo-shortcut.png"/> 
+    <!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/owl.carousel.min.css" />
+    <link rel="stylesheet" href="css/owl.theme.default.min.css" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+  </head>
+  <nav class="navbar navbar-expand-md navbar-light py-3 sticky-top w-100 border" style="background:#fff;">
+  <a class="navbar-brand ml-5" href="index.php"><img src="img/logo.png" class="img-fluid w-50"></a>
+  <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav mr-auto">
+      <a class="nav-link active text-dark pl-4" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-link pl-4" href="#">Portfolio</a>
+      <a class="nav-link pl-4" href="#">Blogs</a>
+	  <a class="nav-link pl-4" href="#">Elements</a>
+      <a class="nav-link pl-4" href="#">Shop</a>
+
+    </div>
+  </div>
+</nav>
